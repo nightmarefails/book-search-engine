efbe14855ac2e67345713d6c7058955e353aedea
Harrison Armstrong <harrisonarmstrong93@gmail.com>
2021-10-23 21:02:30 -0400

    Added typeDefs for graphql

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express')
+
+const typeDefs = gql`
+
+    type User {
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+
+    type Book {
+        authors: [String]
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        user(userId: ID!):User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String, password: String): User
+        login(username: String!, password: String!): Auth
+        addBook(userId: ID!, authors: [String], description: String, bookId: String!, image: String!, link: String, title: String): Book
+        removeBook(userId: ID!, bookId: String!)
+    }
+`;
+
+module.exports = typeDefs;
